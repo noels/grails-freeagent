38a577e326ca802d1f43cf6dd3dfaf14a7b33929
Noel Sharpe <noels@silex-consulting.com>
2011-06-16 16:46:21 +0100

    upgrade to 1.3.7 and change the way properties are handled.  This means the configuration service is no longer required.

--- grails-app/services/freeagent/ConfigurationService.groovy
+++ grails-app/services/freeagent/ConfigurationService.groovy
@@ -4,35 +4,17 @@ class ConfigurationService {
     def config
     def grailsApplication
 
-    def ConfigurationService(grailsApplication) {
-        this.grailsApplication = grailsApplication;
-        config = grailsApplication.config
-    }
-
-    def ConfigurationService(){
-        /*
-        def CONFIG_LOCATION_APP_HOME = 'app.home.directory'
-        def configPath = ''
-        if (System.properties[CONFIG_LOCATION_APP_HOME]) {
-            configPath = System.properties[CONFIG_LOCATION_APP_HOME]
-        }
-
-        def filePath = new File("${configPath}grails-app/conf/Config.groovy").toURL()
-        config = new ConfigSlurper(System.properties.get('grails.env')).parse(filePath)
-        */
-        //config = application.config
-    }
 
     def getFreeagentUserId() {
-        application.config?.freeagent?.userId
+        grailsApplication.config?.freeagent?.userId
     }
 
     def getFreeagentPassword() {
-        application.config?.freeagent?.password
+        grailsApplication.config?.freeagent?.password
     }
 
     def getFreeagentURI(){
-        application.config?.freeagent?.uri
+        grailsApplication.config?.freeagent?.uri
     }
 
 }
